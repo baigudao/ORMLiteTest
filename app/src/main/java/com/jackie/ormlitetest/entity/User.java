aba33016a8c9b599e82a413e2af6c6ff56b2e679
jackie <971060378@qq.com>
2016-12-19 03:37:00 +0800

    简单应用

---
+++ app/src/main/java/com/jackie/ormlitetest/entity/User.java
@@ -0,0 +1,51 @@
+package com.jackie.ormlitetest.entity;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * Created by Administrator on 2016/12/19.
+ */
+
+@DatabaseTable(tableName = "tb_user")
+public class User {
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = "name")
+    private String name;
+    @DatabaseField(columnName = "desc")
+    private String desc;
+
+    public User() {
+    }
+
+    public User(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+}
